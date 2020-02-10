f149788a872a5d232b49fdeecf318c9501e2bccf
Jan Martinek <honza.martinek@gmail.com>
2020-02-10 18:54:56 +0100

    Paper

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-7355bcf';
+const CACHE = 'cache-929ac62';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
